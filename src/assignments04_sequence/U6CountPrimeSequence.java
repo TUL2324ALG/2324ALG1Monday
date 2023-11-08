d71f80106f5d0b438bcd8639dd510d9c26f975f6
Jana Vitvarova <jana.vitvarova@tul.cz>
2023-11-08 10:45:45 +0100

    Add comments

--- src/assignments04_sequence/U6CountPrimeSequence.java
+++ src/assignments04_sequence/U6CountPrimeSequence.java
@@ -1,48 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package assignments04_sequence;
 
 import java.util.Scanner;
 
 public class U6CountPrimeSequence {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = 0;
+        int num;
+        boolean prime;
         int nPrime = 0;
-        boolean prime = true;
-        System.out.println("zadej kladna cisla (Zadej 0 nebo min abych ukoncit): ");
+        
+        //1. Nacti cisla a zpracuj je
+        System.out.println("Zadej kladna cisla, zadavani ukonci 0 nebo zapornym cislem: ");
         while ((num = sc.nextInt()) > 0) {
+            //1.1. zjisti pro kazde cislo prvociselnost
             prime = true; //reinicializece pro nove num
             if(num == 1){ //limitni pripad, neplati na nej stejna pravidla
                 prime = false;
             }
-            for (int i = 2; i < num; i++) {
+            for (int i = 2; i < num; i++) { //stacilo by jit po num/2, nebo dokonce odmocnina z num
                 if (num % i == 0) {
                     prime = false;
                     break;
                 }
             }
+            //1.2. pokud je cislo prvocislo, zvys pocet prvocisel o 1
             if (prime) {
-
                 nPrime++;
             }
-
         }
+        //2. Vypis pocet prvocisel
         System.out.println(nPrime);
-
-//            if (prime) {
-//
-//                nPrime++;
-//            }
-//            System.out.println(nPrime);
     }
-
 }
 
 
